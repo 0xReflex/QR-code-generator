217d0efe6e1f6998c8675ea96b8094d0f37d3d64
0xReflex <58839586+0xReflex@users.noreply.github.com>
2020-04-22 18:22:29 +0530

    Update qrcodegenerator.py

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,5 +1,5 @@
-import qrcode
+import qrcode#importing qrcode in this program
 
-print('what text you want to save')
-img = qrcode.make(input(':'))
-img.save('qr.jpeg')
+print('what text you want to save')# printing the stff..
+img = qrcode.make(input(':'))# taking the input and just giving it to the program
+img.save('qr.jpeg')# now saving the file as QR.jpeg """ you can change the extention to png (I never tested other extention exept png) """ 
